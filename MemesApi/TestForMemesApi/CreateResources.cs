0dd5b5e5bf62aa6b74312e4d4114546eb2082f12
Osmany Gonzalez de Juana <osmani.gonzalez@colba.es>
2021-10-19 14:36:57 +0200

    New tests

--- MemesApi/TestForMemesApi/CreateResources.cs
+++ MemesApi/TestForMemesApi/CreateResources.cs
@@ -9,17 +9,16 @@ namespace TestForMemesApi
     public class CreateResources : IDisposable
     {
         public MockMemeThumbRepository Meme { get; private set; }
-
-        //public JsonElement Json { get; private set; }
-
         public List<MemeThumbnail> Th { get; }
+        public MemeThumbnail Temp1 { get; set; }
+        
+        public MemeThumbnail Temp2 { get; set; }
 
 
         public CreateResources()
         {
             Meme = new MockMemeThumbRepository();
-            // var temp = JsonSerializer.Serialize(th);
-            // Json = JsonSerializer.Deserialize<JsonElement>(temp);
+            
             Th = new()
             {
                 new MemeThumbnail()
@@ -33,12 +32,27 @@ namespace TestForMemesApi
                     Thumb = "ThumbUri_test"
                 }
             };
+            
+            Temp1 = new MemeThumbnail()
+            {
+                MemeId = 1, Name = "Soccer meme", Description = "About the last journey in UCL", Width = 400,
+                Height = 600, Original = "URI_1", Thumb = "ThumbUri_1"
+            };
+            
+            Temp2 = new MemeThumbnail()
+            {
+                MemeId = 500, Name = "Weird meme"
+            };
 
         }
         
+
         public void Dispose()
         {
             GC.SuppressFinalize(Meme);
+            GC.SuppressFinalize(Th);
+            GC.SuppressFinalize(Temp1);
+            
         }
     }
 }
--- MemesApi/TestForMemesApi/ListControllerShould.cs
+++ MemesApi/TestForMemesApi/ListControllerShould.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json;
-using MemesApi.Controllers;
-using MemesApi.Models;
 using Xunit;
 
 namespace TestForMemesApi
@@ -19,15 +14,15 @@ namespace TestForMemesApi
             _res = resources;
         }
         
+        
         [Fact]
         public void InitCorrectly()
         {
-            //Assert.Equal(4, _res.Meme.MyList.Count);
             Assert.NotEmpty(_res.Meme.MyList);
         }
 
         [Fact]
-        public void Create()
+        public void CreateItem()
         {
             _res.Meme.AddItem(_res.Th);
             var ff = _res.Meme.MyList.FirstOrDefault(m => m.Name == "Test meme");
@@ -35,5 +30,30 @@ namespace TestForMemesApi
            Assert.Contains(ff, _res.Meme.MyList);
 
         }
+        
+        [Fact]
+        public void GetItemById()
+        {
+            Assert.NotStrictEqual(_res.Meme.GetMemeById(0), _res.Temp1);
+        }
+        
+        [Fact]
+        public void PutItem()
+        {
+            _res.Meme.MyList[0] = _res.Temp2;
+            Assert.Equal(_res.Meme.GetMemeById(0), _res.Temp2);
+            Assert.NotEqual(_res.Meme.GetMemeById(0), _res.Temp1);
+        }
+        
+        [Fact]
+        public void DeleteItem()
+        {
+            var counter = _res.Meme.MyList.Count - 1;
+            _res.Meme.MyList.RemoveAt(1);
+            
+            Assert.Equal(counter, _res.Meme.MyList.Count );
+            
+        }
+        
     }
 }
